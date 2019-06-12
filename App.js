076a65ba806a4fc2fd576ceb6d69852e8d511e89
belclei <belclei@gmail.com>
2019-06-11 22:09:01 -0300

    The components were built in a static version

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+
+import EditableTimer from "./components/EditableTimer";
+import ToggleableTimerForm from "./components/ToggleableTimerForm";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm isOpen={false} />
+          <EditableTimer
+            id="1"
+            title="Cortar a Grama"
+            project="Tarefas Domésticas"
+            elapsed="8986300"
+            isRunninng
+          />
+          <EditableTimer
+            id="2"
+            title="Estudar Inglês"
+            project="Idiomas"
+            elapsed="3890985"
+            editFormOpen
+          />
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Appcontainer: {
+    flex: 1
   },
+  titleContainer: {
+    paddingTop: 35,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#D6D7DA"
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  timerList: {
+    paddingBottom: 15
+  }
 });
